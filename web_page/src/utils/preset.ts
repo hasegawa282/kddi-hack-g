79cbaff5f870bfa660577b74b082595ad33f75f6
hasegawa282 <masaki.hasegawa@i-d.ai>
2022-07-10 12:43:48 +0900

    friends名称変更

--- web_page/src/utils/preset.ts
+++ web_page/src/utils/preset.ts
@@ -1,11 +1,11 @@
 // プリセット
 export const new_presets = {
   a_preset: {
-     friends:["こんどう","hase","chika","zaima","てつまる","sora"],
+     friends:["こんどう","はせ","ちか","ざいま","てつまる","そら"],
      events:["海","BBQ","肝試し","グランピング","花火","宿題","旅行"],   
   },
   b_preset: {
-    friends:["こんどう","hase","chika","zaima","てつまる","sora"],
+    friends:["こんどう","はせ","ちか","ざいま","てつまる","そら"],
     events:["海","卓球","バドミントン","テニス","バスケットボール","宿題","旅行"]
   }
 }
